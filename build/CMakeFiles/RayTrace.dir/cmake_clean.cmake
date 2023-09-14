c5a05b096e225ff0de3050b0fd4eb344b3bf8efa
shujaK <khwajashuja@gmail.com>
2023-09-14 17:44:44 -0400

    CMake configuration

---
+++ build/CMakeFiles/RayTrace.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTrace.dir/main.cpp.obj"
+  "CMakeFiles/RayTrace.dir/main.cpp.obj.d"
+  "RayTrace.exe"
+  "RayTrace.exe.manifest"
+  "RayTrace.pdb"
+  "libRayTrace.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
