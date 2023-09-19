7d7a207971429dd897294d0c1af393470e998b09
shujaK <khwajashuja@gmail.com>
2023-09-19 01:21:00 -0400

    implemented hittable_list

---
+++ src/hittable_list.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "hittable.h"
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class hittable_list : public hittable {
+    public:
+        // base class
+        std::vector<shared_ptr<hittable>> objects;
+
+        // default and parameterized constructors
+        hittable_list() {}
+        hittable_list(shared_ptr<hittable> object) {add(object);}
+
+        void add(shared_ptr<hittable> object){
+            objects.push_back(object);
+        }
+
+        bool hit(const ray& r, double r_tmin, double r_tmax, hit_record& rec) const override {
+            hit_record temp_rec;
+            bool hit_anything = false;
+            auto closest = r_tmax;
+
+            for (const auto& object: objects) {
+                if (object->hit(r, r_tmin, r_tmax, temp_rec)) {
+                    hit_anything = true;
+                    closest = temp_rec.t;
+                    rec = temp_rec;
+                }
+            }
+
+            return hit_anything;
+        }
+};
